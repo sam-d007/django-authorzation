35e768ccbba9a89fd09aef52282a968dcd502db5
dynamo <saumitra.dobhal@aitouch.in>
2019-05-24 17:45:00 +0530

    initial commit

---
+++ auth_project/auth_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form':form})    
